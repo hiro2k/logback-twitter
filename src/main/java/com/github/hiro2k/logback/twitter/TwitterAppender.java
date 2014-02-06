d60cf538aae0f22cab3088db739dabb67dbf5433
Hugo Garza <hiro2k@gmail.com>
2014-02-06 00:29:44 -0600

    Implemented and Tested!
    I implemented the actual Appender and created some unit tests to make
    sure that it would work. I think I still need to test that Joran loads
    the right properties from the file and then actually test a real post to
    twitter.

--- src/main/java/com/github/hiro2k/logback/twitter/TwitterAppender.java
+++ src/main/java/com/github/hiro2k/logback/twitter/TwitterAppender.java
@@ -15,19 +15,158 @@
  */
 package com.github.hiro2k.logback.twitter;
 
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
 import ch.qos.logback.core.AppenderBase;
+import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.status.ErrorStatus;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
+ * Logback {@link Appender} to write log data to Twitter
+ * <p>
+ * To configure, add an appender element referring to this class in your {@code
+ * logback.xml} file. The following properties must be set:
+ * <ul>
+ * <li>consumerKey: Twitter Consumer key
+ * <li>consumerSecret: Twitter Consumer secret
+ * <li>accessToken: Twitter Access token
+ * <li>accessSecret: Twitter Access token secret
+ * </ul>
+ * The easiest way to obtain these 4 values is to create an application at:
+ * {@link https://dev.twitter.com/}
  *
  * @author Hugo Garza <hiro2k@gmail.com>
  */
-public class TwitterAppender<E> extends AppenderBase {
+public class TwitterAppender extends AppenderBase<ILoggingEvent> {
 
-    ConfigurationBuilder cb = new ConfigurationBuilder();
+    private static final String DEFAULT_PATTERN = "%logger{0} - %m%n%ex{short}";
+    // required properties
+    private String consumerKey;
+    private String consumerSecret;
+    private String accessToken;
+    private String accessSecret;
+    // optional properties
+    private Layout<ILoggingEvent> layout;
+    private Twitter twitter;
+
+    public String getConsumerKey() {
+        return consumerKey;
+    }
+
+    public void setConsumerKey(String consumerKey) {
+        this.consumerKey = consumerKey;
+    }
+
+    public String getConsumerSecret() {
+        return consumerSecret;
+    }
+
+    public void setConsumerSecret(String consumerSecret) {
+        this.consumerSecret = consumerSecret;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessSecret() {
+        return accessSecret;
+    }
+
+    public void setAccessSecret(String accessSecret) {
+        this.accessSecret = accessSecret;
+    }
+
+    public Layout<ILoggingEvent> getLayout() {
+        return layout;
+    }
+
+    public void setLayout(Layout<ILoggingEvent> layout) {
+        this.layout = layout;
+    }
+
+    /**
+     * Sets the Twitter client to use, it's package private to facilitate
+     * testing.
+     *
+     * @param twitter
+     */
+    void setTwitter(Twitter twitter) {
+        this.twitter = twitter;
+    }
 
     @Override
-    protected void append(Object e) {        
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void start() {
+        boolean requiredPropsSet = true;
+        if (accessToken == null) {
+            addStatus(new ErrorStatus("Access token not set", this));
+            requiredPropsSet = false;
+        }
+        if (accessSecret == null) {
+            addStatus(new ErrorStatus("Access secret not set", this));
+            requiredPropsSet = false;
+        }
+        if (consumerKey == null) {
+            addStatus(new ErrorStatus("Consumer key not set", this));
+            requiredPropsSet = false;
+        }
+        if (consumerSecret == null) {
+            addStatus(new ErrorStatus("Consumer Secret not set", this));
+            requiredPropsSet = false;
+        }
+        if (!requiredPropsSet && twitter == null) {
+            addWarn("Appender not started because the required properties weren't all set.");
+            return;
+        }
+        if (layout == null) {
+            layout = makeDefaultPatternLayout();
+        }
+        if (twitter == null) {
+            ConfigurationBuilder configBuilder = new ConfigurationBuilder();
+            configBuilder.setOAuthConsumerKey(consumerKey).setOAuthConsumerSecret(consumerSecret).setOAuthAccessToken(accessToken).setOAuthAccessTokenSecret(accessSecret);
+            TwitterFactory tf = new TwitterFactory(configBuilder.build());
+            this.twitter = tf.getInstance();
+        }
+        super.start();
+    }
+
+    /**
+     * Creates the default PatternLayout in case the user doesn't supply one.
+     *
+     * @return the PatternLayout with pattern DEFAULT_PATTERN
+     */
+    protected PatternLayout makeDefaultPatternLayout() {
+        PatternLayout pl = new PatternLayout();
+        pl.setPattern(DEFAULT_PATTERN);
+        return pl;
+    }
+
+    /**
+     * Updates the status message of the twitter account with the eventObject
+     * formatted as defined by layout.
+     *
+     * @param eventObject the event to be posted on twitter.
+     */
+    @Override
+    protected void append(ILoggingEvent eventObject) {
+        if (layout == null || twitter == null) {
+            addError("layout or twitter are not set.");
+            return;
+        }
+        String message = layout.doLayout(eventObject);
+        try {
+            twitter.updateStatus(message);
+        } catch (TwitterException ex) {
+            addWarn("Message was not posted to twitter.", ex);
+        }
     }
 }
---
+++ src/test/java/com/github/hiro2k/logback/twitter/TwitterAppenderTest.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright 2014 Hugo Garza <hiro2k@gmail.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.github.hiro2k.logback.twitter;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.classic.spi.LoggingEventVO;
+import ch.qos.logback.core.Context;
+import ch.qos.logback.core.ContextBase;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+
+/**
+ *
+ * @author Hugo Garza <hiro2k@gmail.com>
+ */
+public class TwitterAppenderTest {
+
+    Context context = new ContextBase();
+
+    public TwitterAppenderTest() {
+    }
+
+    /**
+     * Test of start method, of class TwitterAppender. This shouldn't start
+     * because none of the required twitter keys are set.
+     */
+    @Test
+    public void testNoStart() {
+        System.out.println("no start");
+        TwitterAppender instance = mock(TwitterAppender.class);
+        doCallRealMethod().when(instance).start();
+        instance.start();
+        verify(instance, never()).makeDefaultPatternLayout();
+    }
+
+    /**
+     * Test of start method, of class TwitterAppender. This should start because
+     * all of the required twitter keys are set.
+     */
+    @Test
+    public void testStartProperties() {
+        System.out.println("start properties");
+        // Spy is easier than callReal
+        TwitterAppender instance = spy(new TwitterAppender());
+        instance.setAccessSecret("");
+        instance.setAccessToken("");
+        instance.setConsumerKey("");
+        instance.setConsumerSecret("");
+        instance.start();
+        verify(instance, times(1)).makeDefaultPatternLayout();
+    }
+
+    /**
+     * Test of start method, of class TwitterAppender. This should start because
+     * the twitter client is mocked.
+     */
+    @Test
+    public void testStartTwitter() {
+        System.out.println("start twitter");
+        TwitterAppender instance = spy(new TwitterAppender());
+        instance.setTwitter(mock(Twitter.class));
+        doCallRealMethod().when(instance).start();
+        instance.start();
+        verify(instance, times(1)).makeDefaultPatternLayout();
+    }
+
+    /**
+     * Test of append method, of class TwitterAppender. This should log an error
+     * because start was not called so twitter and the layout are null.
+     */
+    @Test
+    public void testAppendNotStarted() {
+        System.out.println("append not started");
+        // LoggingEventVO is an empty event, useful for testing.
+        ILoggingEvent eventObject = new LoggingEventVO();
+        TwitterAppender instance = mock(TwitterAppender.class);
+        instance.setContext(context);
+        doCallRealMethod().when(instance).append(eventObject);
+        instance.append(eventObject);
+        verify(instance, times(1)).addError("layout or twitter are not set.");
+    }
+
+    /**
+     * Test of append method, of class TwitterAppender. This should pass because
+     * start was called and twitter was set.
+     */
+    @Test
+    public void testAppendStarted() throws TwitterException {
+        System.out.println("append");
+        Twitter twitter = mock(Twitter.class);
+        // LoggingEventVO is an empty event, useful for testing.
+        ILoggingEvent eventObject = new LoggingEventVO();
+        TwitterAppender instance = new TwitterAppender();
+        instance.setTwitter(twitter);
+        instance.setContext(context);
+        instance.start();
+        instance.append(eventObject);
+        verify(twitter, times(1)).updateStatus("");
+    }
+
+    /**
+     * Test of append method, of class TwitterAppender. This should pass because
+     * start was called and twitter was set.
+     */
+    @Test
+    public void testAppendTwitterException() throws TwitterException {
+        System.out.println("append exception");
+        Twitter twitter = mock(Twitter.class);
+        TwitterException exception = new TwitterException("test");
+        doThrow(exception).when(twitter).updateStatus("");
+        // LoggingEventVO is an empty event, useful for testing.
+        ILoggingEvent eventObject = new LoggingEventVO();
+        TwitterAppender instance = spy(new TwitterAppender());
+        instance.setTwitter(twitter);
+        instance.setContext(context);
+        instance.start();
+        instance.append(eventObject);
+        verify(twitter, times(1)).updateStatus("");
+        verify(instance, times(1)).addWarn("Message was not posted to twitter.", exception);
+    }
+}
